82f79ee42a082bf79d22680272991b5b72e02c8e
Andrey Galkin <galk.and.leo@gmail.com>
2023-07-19 16:33:16 +0300

    kittyBot init

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+import os
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+from dotenv import load_dotenv
+
+load_dotenv()
+secret_token = os.getenv('TOKEN')
+
+updater = Updater(token=secret_token)
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+
+def get_new_image():
+    response = requests.get(URL).json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+def new_cat(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image())
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+    context.bot.send_message(chat_id=chat.id,
+                             text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+                             reply_markup=button)
+
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+
+
+# Метод start_polling() запускает процесс polling,
+# приложение начнёт отправлять регулярные запросы для получения обновлений.
+updater.start_polling()
+# Бот будет работать до тех пор, пока не нажмете Ctrl-C
+updater.idle()
